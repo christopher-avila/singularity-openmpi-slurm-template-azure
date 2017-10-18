0079c434ea8a7efd5fa05c220499947d0b757cf5
christopher <christopher@rstor.io>
2017-10-18 10:10:41 +0200

    modified main.sh

--- main.sh
+++ main.sh
@@ -32,107 +32,107 @@ TEMPLATE_BASE=$9
 
 if [[ `which sinfo &>/dev/null; echo $?` -eq 0 ]]
 then
-  if [[ `which singularity &>/dev/null; echo $?` -eq 0 ]]
-  then
-    exit 0
-  else
-    ./$TEMPLATE_BASE/install-singularity.sh
-    exit 0
-  fi
-else
-  # Update master node
-  echo $MASTER_IP $MASTER_NAME >> /etc/hosts
-  echo $MASTER_IP $MASTER_NAME > /tmp/hosts.$$
-
-  # Update ssh config file to ignore unknow host
-  # Note all settings are for azureuser, NOT root
-  sudo -u $ADMIN_USERNAME sh -c "mkdir /home/$ADMIN_USERNAME/.ssh/;echo Host worker\* > /home/$ADMIN_USERNAME/.ssh/config; echo StrictHostKeyChecking no >> /home/$ADMIN_USERNAME/.ssh/config; echo UserKnownHostsFile=/dev/null >> /home/$ADMIN_USERNAME/.ssh/config"
-
-  # Generate a set of sshkey under /honme/azureuser/.ssh if there is not one yet
-  if ! [ -f /home/$ADMIN_USERNAME/.ssh/id_rsa ]; then
-      sudo -u $ADMIN_USERNAME sh -c "ssh-keygen -f /home/$ADMIN_USERNAME/.ssh/id_rsa -t rsa -N ''"
-  fi
-
-  # Install sshpass to automate ssh-copy-id action
-  sudo apt-get install sshpass -y >> /tmp/azuredeploy.log.$$ 2>&1
-
-  # Loop through all worker nodes, update hosts file and copy ssh public key to it
-  # The script make the assumption that the node is called %WORKER+<index> and have
-  # static IP in sequence order
-  i=0
-  while [ $i -lt $NUM_OF_VM ]
-  do
-     workerip=`expr $i + $WORKER_IP_START`
-     echo 'I update host - '$WORKER_NAME$i >> /tmp/azuredeploy.log.$$ 2>&1
-     echo $WORKER_IP_BASE$workerip $WORKER_NAME$i >> /etc/hosts
-     echo $WORKER_IP_BASE$workerip $WORKER_NAME$i >> /tmp/hosts.$$
-     sudo -u $ADMIN_USERNAME sh -c "sshpass -p '$ADMIN_PASSWORD' ssh-copy-id $WORKER_NAME$i"
-     i=`expr $i + 1`
-  done
-
-  # Install SLURM on master node
-  ###################################
-
-  # Install the package
-  sudo apt-get update >> /tmp/azuredeploy.log.$$ 2>&1
-  sudo chmod g-w /var/log >> /tmp/azuredeploy.log.$$ 2>&1 # Must do this before munge will generate key
-  sudo apt-get install build-essential slurm-llnl -y >> /tmp/azuredeploy.log.$$ 2>&1
-
-  # Download slurm.conf and fill in the node info
-  SLURMCONF=/tmp/slurm.conf.$$
-  wget $TEMPLATE_BASE/slurm.template.conf -O $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
-  sed -i -- 's/__MASTERNODE__/'"$MASTER_NAME"'/g' $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
-  lastvm=`expr $NUM_OF_VM - 1`
-  sed -i -- 's/__WORKERNODES__/'"$WORKER_NAME"'[0-'"$lastvm"']/g' $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
-  sudo cp -f $SLURMCONF /etc/slurm-llnl/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
-  sudo chown slurm /etc/slurm-llnl/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
-  sudo chmod o+w /var/spool # Write access for slurmctld log. Consider switch log file to another location
-  sudo -u slurm /usr/sbin/slurmctld >> /tmp/azuredeploy.log.$$ 2>&1 # Start the master daemon service
-  sudo munged --force >> /tmp/azuredeploy.log.$$ 2>&1 # Start munged
-  sudo slurmd >> /tmp/azuredeploy.log.$$ 2>&1 # Start the node
-
-  # Install slurm on all nodes by running apt-get
-  # Also push munge key and slurm.conf to them
-  echo "Prepare the local copy of munge key" >> /tmp/azuredeploy.log.$$ 2>&1
-
-  mungekey=/tmp/munge.key.$$
-  sudo cp -f /etc/munge/munge.key $mungekey
-  sudo chown $ADMIN_USERNAME $mungekey
-
-  echo "Start looping all workers" >> /tmp/azuredeploy.log.$$ 2>&1
-
-  i=0
-  while [ $i -lt $NUM_OF_VM ]
-  do
-     worker=$WORKER_NAME$i
-
-     echo "SCP to $worker"  >> /tmp/azuredeploy.log.$$ 2>&1
-     sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/azuredeploy.log.$$ 2>&1
-     sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
-     sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts >> /tmp/azuredeploy.log.$$ 2>&1
-
-     echo "Remote execute on $worker" >> /tmp/azuredeploy.log.$$ 2>&1
-     sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker >> /tmp/azuredeploy.log.$$ 2>&1 << 'ENDSSH1'
-        sudo sh -c "cat /tmp/hosts >> /etc/hosts"
-        sudo chmod g-w /var/log
-        sudo apt-get update
-        sudo apt-get install build-essential slurm-llnl -y
-        sudo cp -f /tmp/munge.key /etc/munge/munge.key
-        sudo chown munge /etc/munge/munge.key
-        sudo chgrp munge /etc/munge/munge.key
-        sudo rm -f /tmp/munge.key
-        sudo /usr/sbin/munged --force # ignore egregrious security warning
-        sudo cp -f /tmp/slurm.conf /etc/slurm-llnl/slurm.conf
-        sudo chown slurm /etc/slurm-llnl/slurm.conf
-        sudo slurmd
-  ENDSSH1
-
-     i=`expr $i + 1`
-  done
-  rm -f $mungekey
-
+  exit 0
 fi
 
+# Update master node
+echo $MASTER_IP $MASTER_NAME >> /etc/hosts
+echo $MASTER_IP $MASTER_NAME > /tmp/hosts.$$
+
+# Update ssh config file to ignore unknow host
+# Note all settings are for azureuser, NOT root
+sudo -u $ADMIN_USERNAME sh -c "mkdir /home/$ADMIN_USERNAME/.ssh/;echo Host worker\* > /home/$ADMIN_USERNAME/.ssh/config; echo StrictHostKeyChecking no >> /home/$ADMIN_USERNAME/.ssh/config; echo UserKnownHostsFile=/dev/null >> /home/$ADMIN_USERNAME/.ssh/config"
+
+# Generate a set of sshkey under /honme/azureuser/.ssh if there is not one yet
+if ! [ -f /home/$ADMIN_USERNAME/.ssh/id_rsa ]; then
+    sudo -u $ADMIN_USERNAME sh -c "ssh-keygen -f /home/$ADMIN_USERNAME/.ssh/id_rsa -t rsa -N ''"
+fi
+
+# Install sshpass to automate ssh-copy-id action
+sudo apt-get install sshpass -y >> /tmp/azuredeploy.log.$$ 2>&1
+
+# Loop through all worker nodes, update hosts file and copy ssh public key to it
+# The script make the assumption that the node is called %WORKER+<index> and have
+# static IP in sequence order
+i=0
+while [ $i -lt $NUM_OF_VM ]
+do
+   workerip=`expr $i + $WORKER_IP_START`
+   echo 'I update host - '$WORKER_NAME$i >> /tmp/azuredeploy.log.$$ 2>&1
+   echo $WORKER_IP_BASE$workerip $WORKER_NAME$i >> /etc/hosts
+   echo $WORKER_IP_BASE$workerip $WORKER_NAME$i >> /tmp/hosts.$$
+   sudo -u $ADMIN_USERNAME sh -c "sshpass -p '$ADMIN_PASSWORD' ssh-copy-id $WORKER_NAME$i"
+   i=`expr $i + 1`
+done
+
+# Install SLURM on master node
+###################################
+
+# Install the package
+sudo apt-get update >> /tmp/azuredeploy.log.$$ 2>&1
+sudo chmod g-w /var/log >> /tmp/azuredeploy.log.$$ 2>&1 # Must do this before munge will generate key
+sudo apt-get install build-essential slurm-llnl -y >> /tmp/azuredeploy.log.$$ 2>&1
+
+# Download slurm.conf and fill in the node info
+SLURMCONF=/tmp/slurm.conf.$$
+wget $TEMPLATE_BASE/slurm.template.conf -O $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
+sed -i -- 's/__MASTERNODE__/'"$MASTER_NAME"'/g' $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
+lastvm=`expr $NUM_OF_VM - 1`
+sed -i -- 's/__WORKERNODES__/'"$WORKER_NAME"'[0-'"$lastvm"']/g' $SLURMCONF >> /tmp/azuredeploy.log.$$ 2>&1
+sudo cp -f $SLURMCONF /etc/slurm-llnl/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
+sudo chown slurm /etc/slurm-llnl/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
+sudo chmod o+w /var/spool # Write access for slurmctld log. Consider switch log file to another location
+sudo -u slurm /usr/sbin/slurmctld >> /tmp/azuredeploy.log.$$ 2>&1 # Start the master daemon service
+sudo munged --force >> /tmp/azuredeploy.log.$$ 2>&1 # Start munged
+sudo slurmd >> /tmp/azuredeploy.log.$$ 2>&1 # Start the node
+
+# Install slurm on all nodes by running apt-get
+# Also push munge key and slurm.conf to them
+echo "Prepare the local copy of munge key" >> /tmp/azuredeploy.log.$$ 2>&1
+
+mungekey=/tmp/munge.key.$$
+sudo cp -f /etc/munge/munge.key $mungekey
+sudo chown $ADMIN_USERNAME $mungekey
+
+echo "Start looping all workers" >> /tmp/azuredeploy.log.$$ 2>&1
+
+i=0
+while [ $i -lt $NUM_OF_VM ]
+do
+   worker=$WORKER_NAME$i
+
+   echo "SCP to $worker"  >> /tmp/azuredeploy.log.$$ 2>&1
+   sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/azuredeploy.log.$$ 2>&1
+   sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
+   sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts >> /tmp/azuredeploy.log.$$ 2>&1
+
+   echo "Remote execute on $worker" >> /tmp/azuredeploy.log.$$ 2>&1
+   sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker >> /tmp/azuredeploy.log.$$ 2>&1 << 'ENDSSH1'
+      sudo sh -c "cat /tmp/hosts >> /etc/hosts"
+      sudo chmod g-w /var/log
+      sudo apt-get update
+      sudo apt-get install build-essential slurm-llnl -y
+      sudo cp -f /tmp/munge.key /etc/munge/munge.key
+      sudo chown munge /etc/munge/munge.key
+      sudo chgrp munge /etc/munge/munge.key
+      sudo rm -f /tmp/munge.key
+      sudo /usr/sbin/munged --force # ignore egregrious security warning
+      sudo cp -f /tmp/slurm.conf /etc/slurm-llnl/slurm.conf
+      sudo chown slurm /etc/slurm-llnl/slurm.conf
+      sudo slurmd
+ENDSSH1
+
+   i=`expr $i + 1`
+done
+rm -f $mungekey
+
 # Restart slurm service on all nodes
+
+if [[ `which singularity &>/dev/null; echo $?` -eq 0 ]]
+then
+  echo 'installed'
+else
+  ./$TEMPLATE_BASE/install-singularity.sh
+fi
 
 exit 0
