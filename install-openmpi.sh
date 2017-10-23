5b5fa3302a33547a4d9ea0cb2aec61fe5136bed6
Christopher <christopher@rstor.io>
2017-10-23 17:21:00 +0200

    Update install-openmpi.sh

--- install-openmpi.sh
+++ install-openmpi.sh
@@ -9,7 +9,6 @@ tar xvf openmpi-*.tar.gz
 cd openmpi-$VERSION.0
 ./configure --prefix="/home/$USER/.openmpi"
 make; sudo make install
-export PATH="$PATH:/home/$USER/.openmpi/bin";
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/$USER/.openmpi/lib/"
 echo export PATH="$PATH:/home/$USER/.openmpi/bin" >> /home/$USER/.bashrc ;
-echo export LD_LIBRARY_PATH="$LD_LIBRARY_PA*TH:/home/$USER/.openmpi/lib/" >> /home/$USER/.bashrc
+echo export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/$USER/.openmpi/lib/" >> /home/$USER/.bashrc
+bash
