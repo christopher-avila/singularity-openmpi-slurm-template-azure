e98dee9961f7f09bd339e3833ddd1a2a31886917
christopher <christopher@rstor.io>
2017-10-25 10:08:59 +0200

    moved files into directories

---
+++ ubuntu/main-no-rm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Basic info
+date > /tmp/azuredeploy.log.$$ 2>&1
+whoami >> /tmp/azuredeploy.log.$$ 2>&1
+echo $@ >> /tmp/azuredeploy.log.$$ 2>&1
+
+#install singularity
+sudo apt-get update >> /tmp/azuredeploy.log.$$ 2>&1
+sudo apt-get install build-essenti openmpi-bin=1.10.2-8ubuntu1 -y >> /tmp/azuredeploy.log.$$ 2>&1
+
+#install singularity
+TEMPLATE_BASE=$1
+wget $TEMPLATE_BASE/install-singularity.sh
+sudo chmod +x install-singularity.sh
+./install-singularity.sh  >> /tmp/azuredeploy.log.$$ 2>&1
+sudo mv install-singularity.sh /opt/
+
+
+exit 0
