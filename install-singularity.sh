045f48c49cded7aecc0e8c4ae1481c226e815a31
Christopher <christopher@rstor.io>
2018-01-25 16:00:16 +0100

    Update install-singularity.sh
    updated singularity version to 2.4.2

--- install-singularity.sh
+++ install-singularity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=2.3.2
+VERSION=2.4.2
 cd /opt/
 wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
 tar xvf singularity-$VERSION.tar.gz
